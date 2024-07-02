7e74e25a9996e252dcca0a89857e3d457e8c7799
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-02 22:35:09 +0930

    add playRound function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// function get computer choice
+let humanSelection = getHumanChoice();
+let compSelection = getComputerChoice();
 
 function getComputerChoice() {
   let compChoice = Math.random();
@@ -13,14 +14,57 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt("Please choose rock, paper or scissors");
+  let humanChoice;
 
+  while (true) {
+    humanChoice = prompt("Please choose rock, paper or scissors").toLowerCase();
+
+    if (
+      humanChoice == "rock" ||
+      humanChoice == "paper" ||
+      humanChoice == "scissors"
+    ) {
+      break;
+    } else alert("Please enter a valid value");
+  }
   return humanChoice;
 }
+
+function playRound(compChoice, humanChoice) {
+let humanWon = 'You Won!'
+let humanLost = 'You lost! Better luck next time'
+
+let humanScore = 0;
+let compScore = 0;
+
+  if (compChoice === humanChoice) {
+    alert("its a tie");
+  } else if (compChoice === "scissors" && humanChoice === "paper") {
+    alert(humanLost);
+    compScore++
+  } else if (compChoice === "paper" && humanChoice === "rock") {
+    alert(humanLost);
+    compScore++
+  } else if (compChoice === "rock" && humanChoice === "scissors") {
+    alert(humanLost);
+    compScore++
+  }  else if (compChoice === "scissors" && humanChoice === "rock") {
+    alert(humanWon);
+    humanScore++
+  } else if (compChoice === "paper" && humanChoice === "scissors") {
+    alert(humanWon);
+    humanScore++
+  } else if (compChoice === "rock" && humanChoice === "scissors") {
+    alert(humanWon);
+    humanScore++
+  }
+}
+
+
+playRound(compSelection, humanSelection)
 
 // function get human choice
 // hint: use math.random
 
 // main function
 // -evaluate winner
-// -print result to console. pape
