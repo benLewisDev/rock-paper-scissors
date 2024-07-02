bec0efd9cd200574ef3d2207fe1b0e3ffc00c56d
Ben Lewis <104620277+benLewisDev@users.noreply.github.com>
2024-07-02 20:58:30 +0930

    prettify formatting

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
+// function get computer choice
 
+function getComputerChoice() {
+  let compChoice = Math.random();
 
+  if (compChoice <= 0.33) {
+    compChoice = "rock";
+  } else if (compChoice >= 0.66) {
+    compChoice = "scissors";
+  } else compChoice = "paper";
 
-// function get computer choice 
-
-function getComputerChoice () {
-    let compChoice = Math.random();
-    
-    if (compChoice <= 0.33){
-        compChoice = 'rock'
-    }
-    else if (compChoice >= 0.66){
-        compChoice = 'scissors'
-    }
-    else compChoice = 'paper'
-
-    return compChoice
+  return compChoice;
 }
 
-function getHumanChoice () {
-    let humanChoice = prompt('Please choose rock, paper or scissors')
-    
-    return humanChoice
+function getHumanChoice() {
+  let humanChoice = prompt("Please choose rock, paper or scissors");
+
+  return humanChoice;
 }
-
-
 
 // function get human choice
 // hint: use math.random
-
 
 // main function
 // -evaluate winner
